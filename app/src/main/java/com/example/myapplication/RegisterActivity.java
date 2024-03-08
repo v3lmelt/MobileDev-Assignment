58b61452b6c7caf914137c20b981f3fc0474dc62
v3lmelt <143810559+v3lmelt@users.noreply.github.com>
2024-03-08 09:29:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_register);
+    }
+}
